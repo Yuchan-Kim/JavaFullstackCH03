da49746456a2bef077e14c3468dc6f1fb607b5fe
Yuchan Kim <yuchankim0906@gmail.com>
2024-06-27 10:06:18 +0900

    First Commit for the loop

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,13 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		/*
+		 * (6) if(name == "홍길동") 
+		 * 이후 진행 되는 코드가 없다.
+		 */
+
+	}
+
+}
---
+++ src/com/javaex/practice/Ex05.java
@@ -0,0 +1,14 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		int age;
+		
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
